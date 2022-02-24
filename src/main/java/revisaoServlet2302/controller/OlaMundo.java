b338aedc5dc62a7f2df8685e3e73d13df20a811d
Isabela Marques <dev.isabelamarques@gmail.com>
2022-02-23 21:10:24 -0300

    sistema inicial com servlet e jsp

---
+++ src/main/java/revisaoServlet2302/controller/OlaMundo.java
@@ -0,0 +1,49 @@
+package revisaoServlet2302.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class OlaMundo
+ */
+@WebServlet("/OlaMundo")
+public class OlaMundo extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public OlaMundo() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		out.println("<!doctype hmtl>");
+		out.println("<hmtl charset='utf-8'>");
+		out.println("<headl>");
+		out.println("<title> Aula </title> ");
+		out.println("<body>");
+		out.println("<p> Isabela Marques </p>");
+		out.println("</body>");
+		out.println("</html>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
